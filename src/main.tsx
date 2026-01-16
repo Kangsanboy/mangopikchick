31fe3b04b09a0c28dd46d95310cd45901e5f8661
Kangsanboy <128477621+Kangsanboy@users.noreply.github.com>
2026-01-17 02:21:37 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './index.css' // <--- JANGAN SAMPAI HILANG INI!
 
 createRoot(document.getElementById("root")!).render(<App />);
