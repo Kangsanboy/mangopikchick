a2abaaf7aeca7c69155e2299e126687ebdc43181
Kangsanboy <128477621+Kangsanboy@users.noreply.github.com>
2026-02-21 12:03:19 +0700

    Update Karyawan.tsx

--- src/pages/Karyawan.tsx
+++ src/pages/Karyawan.tsx
@@ -297,7 +297,7 @@ const Karyawan = () => {
           
           <div class="header">
            <div class="logo-container">
-                <img src="logo ayam.png" alt="Logo Ayam" style="width: 80px; height: auto; margin-bottom: 10px;">
+                <img src="logo ayam.png" alt="Logo Ayam" style="width: 100px; height: auto; margin-bottom: 10px;">
                 <div class="company-name">PA IYAT BROILER</div>
               <div class="address">Jl. Wr. Lobak, Gandasari, Kec. Katapang,<br>Kab.Bandung 40921</div>
             </div>
